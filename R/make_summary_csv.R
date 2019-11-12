92bf3573e14a549aa26d0decea63288eded5ad2a
Derek T Burk <burkx031@umn.edu>
2019-11-12 12:44:43 -0600

    Another package reference fix

--- R/make_summary_csv.R
+++ R/make_summary_csv.R
@@ -1,5 +1,5 @@
 #' Save summary csv
-#' 
+#'
 #' Save a csv file with the highest FREX or probability words for each topic in a structural topic model
 #' @param stm_model Output from a call to \code{stm}.
 #' @param filename Path to write the file to.
@@ -9,7 +9,7 @@
 
 make_summary_csv <- function(stm_model, filename, type=c("frex", "prob"), n_words=150) {
     word_type <- match.arg(type)
-    words <- labelTopics(stm_model, n=n_words)[[word_type]]
+    words <- stm::labelTopics(stm_model, n=n_words)[[word_type]]
     out <- data.frame(t(words))
     names(out) <- paste0("Topic ", 1:ncol(out))
     write.csv(out, file=filename, row.names=FALSE)
